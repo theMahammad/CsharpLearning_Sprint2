d1094037205da1e746229db301ba91f896a073bf
theMahammad <mehemmedsadigzade@gmail.com>
2023-01-02 17:59:57 +0400

    Day 1

---
+++ ReflectionApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+string path = @"C:\Users\acer\source\repos\CSharpLearning_Sprint2\ReflectionLibrary\bin\Debug\net6.0\ReflectionLibrary.dll";
+Assembly assembly = Assembly.LoadFile(path);
+List<Type> types = assembly.GetExportedTypes().ToList();
+foreach(Type type in types)
+{
+    Console.WriteLine($"Class name : {type.Name}");
+    #region Constructors
+    Console.WriteLine("Its Constructors:");
+    ConstructorInfo[] ctors = type.GetConstructors();
+    foreach (var ctor in ctors)
+    {
+        Console.WriteLine(" " + ctor);
+    }
+    #endregion
+    #region Methods
+    Console.WriteLine("Its methods : ");
+    MethodInfo[] methods = type.GetMethods();
+    foreach (var method in methods)
+    {
+        Console.WriteLine(" " + method);
+    }
+    #endregion
+    #region Properties
+    #endregion
+
+
+    Console.WriteLine("Its properties : ");
+        PropertyInfo[] properties = type.GetProperties();
+    foreach(var property in properties)
+    {
+        Console.WriteLine(" "+ property);
+    }
+    Console.WriteLine("*************************");
+}
---
+++ ReflectionLibrary/Customer.cs
@@ -0,0 +1,22 @@
+﻿namespace ReflectionLibrary
+{
+    public class Customer:Person
+    {
+        public int CustomerNumber { get; set; }
+        public Customer(Guid id, int customerNumber) { 
+        
+            this.Id = id;
+            this.CustomerNumber = customerNumber;
+        }
+        public Customer(Guid id, int customerNumber, string name, string surname) : this(id, customerNumber)
+        {
+            this.Name = name;
+            this.Surname = surname;
+        }
+        public void UpdateCustomerNumber(int customerNumber)
+        {
+            this.CustomerNumber = customerNumber;
+        }
+
+    }
+}
